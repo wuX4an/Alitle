b0aaabc26123b87e2ad2acc978dcf8abfb771a8b
wuX4an <wux4an@outlook.com>
2024-03-08 15:18:03 -0500

    curl install test 1

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "hola"
+
+cd /tmp
+git clone https://github.com/wuX4an/Alitle.git >> ./shit
+sleep 0.25
+cd Alitle
+sudo cp ./setups/ali /bin
+
+mkdir /opt/ali/
+mkdir /opt/ali/bin/
+mkdir /opt/ali/conf/
+
+cp ./setups/bin/* /opt/ali/bin/
+cp ./setups/conf/* /opt/ali/conf/
