b513fed3661acb46861a8148e52af7451f26b1e1
wuX4an <wux4an@tutanota.com>
2024-03-29 13:38:32 -0500

    install.sh out of service

--- install.sh
+++ install.sh
@@ -13,15 +13,17 @@ sudo cp setups/base/ali /bin
 
 sleep 0.25
 
-mkdir /home/$user/Alitle
-mkdir /home/$user/Alitle/bin/
-mkdir /home/$user/Alitle/conf/
-mkdir /home/$user/Alitle/tools/
+mkdir /home/$user/.ali
+mkdir /home/$user/.ali/bin/
+mkdir /home/$user/.ali/conf/
+mkdir /home/$user/.ali/tools/
 
 sleep 0.25
 
-cp ./setups/bin/* /home/$user/Alitle/bin/
-cp ./setups/conf/* /home/$user/Alitle/conf/
-cp ./setups/tools/* /home/$user/Alitle/tools/
+cp ./setups/bin/* /home/$user/.ali/bin/
+cp ./setups/conf/* /home/$user/.ali/conf/
+cp ./setups/tools/* /home/$user/.ali/tools/
 
 COMMENT
+
+echo 'out of service'
