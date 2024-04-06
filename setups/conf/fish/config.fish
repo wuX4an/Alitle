ab915b0f0eb8d5b8b90bea69bdf7165774aa3e6e
wuX4an <wux4an@tutanota.com>
2024-04-05 21:57:49 -0500

    change welcome message

--- setups/conf/fish/config.fish
+++ setups/conf/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set username (whoami)
+
+function fish_greeting                                            
+    set_color cyan; echo "Welcome back $username"
+end
