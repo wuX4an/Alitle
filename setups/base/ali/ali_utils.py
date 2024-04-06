ab48606124f4dedbf7fd11096106d2c6f6fdfc0e
wuX4an <wux4an@tutanota.com>
2024-04-06 11:01:51 -0500

    add comments

--- setups/base/ali/ali_utils.py
+++ setups/base/ali/ali_utils.py
@@ -2,6 +2,10 @@ import os
 import conf
 from time import sleep
 
+# =============================================
+#   FIND CHAIN
+# =============================================
+
 def find_chain(log_path, key, error, loading_message, finish_message, error_message, animation):
     symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
     a = 0
@@ -29,7 +33,9 @@ def find_chain(log_path, key, error, loading_message, finish_message, error_mess
         if find == True:
             break
 
-
+# =============================================
+#   CREATE
+# =============================================
 
 def create(username, password, alpine_version):
 
@@ -49,43 +55,57 @@ def create(username, password, alpine_version):
     find_chain("log", "***","permission denied", "Creating", "Creation Complete", "Something Went Wrong", True)
     
 
-
+# =============================================
+#   SHELL
+# =============================================
 
 def shell(username):
     os.system("nohup sh -c 'docker start alitle' > /dev/null 2>&1&")
     os.system(f'docker exec -it alitle su {username}')
 
-
+# =============================================
+#   START
+# =============================================
 
 def start():
     os.system("nohup sh -c 'docker start alitle' > log 2>&1&")
     find_chain("log", "alitle", "No such container", "Starting", "The container started", "The Container no Exist", True)
 
-
+# =============================================
+#   STOP
+# =============================================
 
 def stop():
     #os.system('docker stop alitle')
     os.system("nohup sh -c 'docker stop alitle' > log 2>&1&")
     find_chain("log", "alitle","No such container", "Stopping", "The container stopped", "The Container no Exist", True)
 
-
+# =============================================
+#   CREATE RESTART
+# =============================================
 
 def restart():
     os.system("nohup sh -c 'docker restart alitle' > log 2>&1&")
     find_chain("log", "alitle","No such container", "Restarting", "The container as been restarted", "The Container no Exist", True)
 
+# =============================================
+#   DELETE
+# =============================================
 
 def delete():
     os.system("nohup sh -c 'docker stop alitle && docker rm alitle' > log 2>&1&")
     find_chain("log", "alitle","No such container", "Removing", "The container as been removed", "The Container no Exist", True)
 
-
+# =============================================
+#   STATUS
+# =============================================
 
 def status():
     os.system('docker stats alitle')
 
-
-
+# =============================================
+#   UNISTALL
+# =============================================
 
 def uninstall():
     os.system('echo -e "***\e[31m Uninstall \e[0m***"')
