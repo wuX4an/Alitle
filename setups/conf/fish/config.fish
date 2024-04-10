ccf4cd3991c0b2e8639df97afe59385e0158a575
wux4an <wux4an@tutanota.com>
2024-04-09 19:55:48 -0500

    remove conf/fish

--- setups/conf/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set username (whoami)
-
-function fish_greeting                                            
-    set_color cyan; echo "Welcome back $username"
-end
